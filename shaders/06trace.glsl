4c16ac449d43bc57904dbd6bc582029eca26b95e
stevejtrettel <stevejtrettel@gmail.com>
2020-10-22 21:05:43 -0700

    working rk4

--- shaders/06trace.glsl
+++ shaders/06trace.glsl
@@ -31,10 +31,53 @@ vec4 vecField(vec4 p){
 
 
 
+
+//structure for the derivative of a tangent vector.
+struct velAcc{
+    vec4 vel;
+    vec4 acc;
+};
+
+
+velAcc add(velAcc s1,velAcc s2){
+    vec4 vel=s1.vel+s2.vel;
+vec4 acc=s1.acc+s2.acc;
+velAcc dState;
+dState.vel=vel;
+dState.acc=acc;
+    return dState;
+}
+
+
+
+velAcc scale(velAcc dState,float k){
+    dState.vel*=k;
+    dState.acc*=k;
+    return dState;
+}
+
+
+Vector nudge(Vector tv, velAcc dState,float step){
+    tv.pos.coords+=dState.vel*step;
+    tv.dir+=dState.acc*step;
+    return tv;
+}
 //----------------------------------------------------------------------------------------------------------------------
 // Marching Through NonConstant Media
 //----------------------------------------------------------------------------------------------------------------------
 
+
+
+velAcc stateDeriv(Vector tv){
+    
+    vec4 pos=tv.pos.coords;
+    vec4 vel=tv.dir;
+    
+ velAcc dState;
+    dState.vel=vel;
+    dState.acc=vecField(pos);
+    return dState;
+}
 
 
 
@@ -75,52 +118,61 @@ void euler(inout Vector tv){
 
 
 
+
+
+
+
 void rk4(inout Vector tv){
     
     //do an iteration of rk4 to the second order equation y''=vector field
     
     //timestep
-    float dt=0.05;
+    float dt=0.1;
     
     //constants computed during the process
-    vec4 k1,k2,k3,k4;
-    vec4 j1,j2,j3,j4;
+    velAcc k1,k2,k3,k4;
     
     //initial conditions
     vec4 y=tv.pos.coords;//position
     vec4 u=tv.dir;//velocity
     
+    Vector temp;
     
     //iteratively step through rk4
     for(int n=0;n<100;n++){
+   
         
-        //compute j1,k2
-        j1=u*dt;
-        k1=vecField(y)*dt;
+        //get the derivative
+        k1=stateDeriv(tv);
+        k1=scale(k1,dt);
         
-        //compute j2,k2
-        j2=(u+j1/2.)*dt;
-        k2=vecField(y+k1/2.)*dt;
+        //move the point a littkle
+        temp=nudge(tv,k1,0.5);
+        k2=stateDeriv(temp);
+        k2=scale(k2,dt);
         
-        //compute j3,k3
-        j3=(u+j2/2.)*dt;
-        k3=vecField(y+k2/2.)*dt;
+        //get k3
+        temp=nudge(tv,k2,0.5);
+        k3=stateDeriv(temp);
+        k3=scale(k3,dt);
         
-        //compute j4,k4
-        j4=(u+j3)*dt;
-        k4=vecField(y+k3)*dt;
+        //get k4
+        temp=nudge(tv,k3,1.);
+        k4=stateDeriv(temp);
+        k4=scale(k4,dt);
         
+        //add up results:
+        velAcc total=add(k1,scale(k2,2.));
+        total=add(total,scale(k3,2.));
+        total=add(total,k4);
         
-        //compute the updated y and u
-        u+=k1/6.+k2/3.+k3/3.+k4/6.;
-        y+=j1/6.+j2/3.+j3/3.+j4/6.;
-            
-//        y+=j1;
-//        u+=k1;
+        tv=nudge(tv,total,1.);
+        
+       
     }
     
     //after the loop, reassemble tv at the endpoint
-    sampletv=Vector(Point(y),u);
+    sampletv=tv;
     
 }
 
